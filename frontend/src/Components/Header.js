b0c97a567c1f5ce38128387a94fbf0c96bc8d406
Jeo Escobar <jeo_escobar@outlook.com>
2021-03-30 23:47:13 -0600

    first interface

---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,41 @@
+import {React} from 'react'
+import { Layout, Menu, Breadcrumb, Button } from 'antd';
+import '../styles/header-styles.css'
+
+const { Header, Content, Footer } = Layout;
+
+const HeaderMain = props =>{
+  
+    return(
+  <Layout>
+    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+        Content
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    <>
+    <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <br />
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
+  </>
+  </Layout>
+    )
+    };
+    export default HeaderMain;
